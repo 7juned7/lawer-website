527b26d65c08d6cd21674f5a0be993c0adca47f8
juned <7juned7@gmail.com>
2026-03-07 12:29:02 +0530

    push

---
+++ src/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.ezeelegal.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ezeelegal.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ezeelegal.in/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
